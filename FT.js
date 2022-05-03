47b2b53359775f70a96738ec4bc5fe58f53cb564
Fabricio Angeloni <fabriangeloni@hotmail.com>
2022-05-03 15:54:15 -0300

    oradores

---
+++ FT.js
@@ -0,0 +1,27 @@
+function resaltar1() {
+    document.getElementById("tjt1").style.backgroundColor="grey";
+    document.getElementById("tjt1").style.color="white"
+}
+function normal1() {
+    document.getElementById("tjt1").style.backgroundColor="white";
+    document.getElementById("tjt1").style.color="black"
+
+}
+function resaltar2() {
+    document.getElementById("tjt2").style.backgroundColor="grey";
+    document.getElementById("tjt2").style.color="white"
+}
+function normal2() {
+    document.getElementById("tjt2").style.backgroundColor="white";
+    document.getElementById("tjt2").style.color="black"
+
+}
+function resaltar3() {
+    document.getElementById("tjt3").style.backgroundColor="grey";
+    document.getElementById("tjt3").style.color="white"
+}
+function normal3() {
+    document.getElementById("tjt3").style.backgroundColor="white";
+    document.getElementById("tjt3").style.color="black"
+
+}
